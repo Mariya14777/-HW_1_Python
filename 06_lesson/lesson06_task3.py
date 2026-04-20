4a49bc78e337e3aaa64a0e236f7947deb814f194
Mariya14777 <mariavoronova533@gmail.com>
2026-04-20 09:16:52 +0300

    Исправлено: стиль PEP8

--- 06_lesson/lesson06_task3.py
+++ 06_lesson/lesson06_task3.py
@@ -1,7 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 
 driver = webdriver.Chrome()
 
@@ -9,22 +8,16 @@ try:
     url = "https://bonigarcia.dev/selenium-webdriver-java/loading-images.html"
     driver.get(url)
 
-    # Ждем появления текста "Done!" (все картинки загружены)
     wait = WebDriverWait(driver, 30)
-    wait.until(EC.text_to_be_present_in_element((By.ID, "text"), "Done!"))
 
-    # Находим картинки только в контейнере
-    selector = "#image-container img"
-    images = driver.find_elements(By.CSS_SELECTOR, selector)
+    # Ждем загрузки третьей картинки (award)
+    third_image = wait.until(
+        lambda d: d.find_element(By.ID, "award")
+        if d.find_element(By.ID, "award").get_attribute("src")
+        else False
+    )
 
-    print(f"Найдено картинок в контейнере: {len(images)}")
-
-    # Выводим src третьей картинки (индекс 2)
-    if len(images) >= 3:
-        third_image_src = images[2].get_attribute("src")
-        print(third_image_src)
-    else:
-        print("Загружено меньше 3-х картинок")
+    print(third_image.get_attribute("src"))
 
 finally:
     driver.quit()
